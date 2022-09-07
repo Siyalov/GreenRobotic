0faff821190226162d25dc13ebf91c37f52bfdf3
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-09-07 12:56:36 +0300

    CanvasBackground

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,6 @@
 
 const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
+const BUBBLE_DENSITY = 100;
 
 function generateDecimalBetween(left, right) {
   return (Math.random() * (left - right) + right).toFixed(2);
@@ -20,9 +21,10 @@ class Bubble {
  
    init() {
       this.color = COLORS[Math.floor(Math.random() * COLORS.length)]
+      this.alpha = generateDecimalBetween(5, 10) / 10;
       this.size = generateDecimalBetween(1, 3);
 
-      this.alpha = generateDecimalBetween(5, 10) / 10;
+      
       this.translateX = generateDecimalBetween(0, this.canvasWidth);
       this.translateY = generateDecimalBetween(0, this.canvasHeight);
 
@@ -42,11 +44,57 @@ class Bubble {
   }
 }
 
-const canvas = document.getElementById("orb-canvas");
 
-const bubbles = [];
-bubbles.push(new Bubble(canvas));
-bubbles.push(new Bubble(canvas));
-bubbles.push(new Bubble(canvas));
+class CanvasBackground {
+   constructor(id) {
+       this.canvas = document.getElementById(id);
+       this.ctx = this.canvas.getContext("2d");
+       this.dpr = window.devicePixelRatio;
+   }
 
-console.log(bubbles);
+   start() {
+      this.canvasSize();
+      this.generateBubbles();
+      this.animate();
+  }
+
+canvasSize() {
+   this.canvas.width = this.canvas.offsetWidth * this.dpr;
+   this.canvas.height = this.canvas.offsetHeight * this.dpr;
+
+   this.ctx.scale(this.dpr, this.dpr);
+}
+
+animate() {
+   this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+
+   this.bubblesList.forEach((bubble) => {
+      bubble.move();
+
+      this.ctx.translate(bubble.translateX, bubble.translateY);
+      this.ctx.beginPath();
+
+      this.ctx.arc(0, 0, bubble.size, 0, 2 * Math.PI);
+      this.ctx.fillStyle = "rgba(" + bubble.color + "," + bubble.alpha + ")";
+      this.ctx.fill();
+
+      this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0);      
+  });
+
+  requestAnimationFrame(this.animate.bind(this));
+}
+
+
+generateBubbles() {
+   this.bubblesList = [];
+   for (let i = 0; i < BUBBLE_DENSITY; i++) {
+       this.bubblesList.push(new Bubble(this.canvas))
+   }
+} 
+}
+
+const canvas = new CanvasBackground("orb-canvas");
+canvas.start();
+
+
+
